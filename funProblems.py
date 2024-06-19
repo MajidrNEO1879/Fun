0f0941c8e2ec152b56dcf94902124e9ee404459b
MajidrNEO1879 <majidrezaeiphy1879@gmail.com>
2024-06-19 21:53:12 +0100

    new solution

--- funProblems.py
+++ funProblems.py
@@ -1,18 +1,14 @@
-from typing import List
-
-
-A = [1,2,3,4,5,]
-#A = [-1, 2, 1, 0, 13, 3, -11]
-A = [0, 1, 2, 3, 5,4]
-ops = 0
-ops2 = 0
-for i in range(1,len(A)):
-    temp = A[i]
-    j = i -1
-    ops2+=1
-    while j>=0 and A[j]>temp:
-        A[j+1], j = A[j], j-1
-        ops+=1
-    A[j+1]=temp
-print (A, ops, ops2)
+import math
+from matplotlib.pyplot import *
+rcParams.update({'font.size': 25, 'font.family': 'serif', 'lines.linewidth': 3})
+figure(figsize=(20, 10))
+xlabel("n")
+max_n = 2000
+x = range(1, max_n)
+y = [100 * n * math.log(n, 2) for n in x]
+plot(x, y, 'r', label='100n*log(n)')
+y = [n ** 2 for n in x]
+plot(x, y, 'g', label='n^2')
+legend(loc=2)
+show()
  
